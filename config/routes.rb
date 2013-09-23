c630f9762dc75e3894ec9212370b409f68172d9a
Gavin Miller <gavingmiller@gmail.com>
2013-09-22 19:30:16 -0600

    Setup proper url endpoint for root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 DebuggingRails::Application.routes.draw do
   resources :actors
 
-
   devise_for :users
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  root :to => 'actors#index'
 end
