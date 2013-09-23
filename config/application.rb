f344f6639fab029b40d64221702e40f866b68ad6
Gavin Miller <gavingmiller@gmail.com>
2013-09-22 19:18:45 -0600

    Added devise installation

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module DebuggingRails
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Prevent the applicaiton from accessing the db on asset compile
+    config.assets.initialize_on_precompile = false
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ DebuggingRails::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Define default mailer for devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
